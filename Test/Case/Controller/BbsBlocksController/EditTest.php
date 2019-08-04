22af7d6f86c447ab34a717e08cdc4d9d1e20253a
s-nakajima <nakajimashouhei@gmail.com>
2019-08-04 19:25:54 +0900

    fix: test: UniteTest修正 https://github.com/NetCommons3/NetCommons3/issues/1468

--- Test/Case/Controller/BbsBlocksController/EditTest.php
+++ Test/Case/Controller/BbsBlocksController/EditTest.php
@@ -66,7 +66,7 @@ class BbsBlocksControllerEditTest extends BlocksControllerEditTest {
 		$frameId = '6';
 		$frameKey = 'frame_3';
 		if ($isEdit) {
-			$blockId = '4';
+			$blockId = '2';
 			$blockKey = 'block_2';
 			$bbsId = '3';
 			$bbsKey = 'bbs_2';
